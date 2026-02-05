75959a483b87050fa7492ee73b01c5d5cb06bfd5
delterr <hasanahmadcodes@gmail.com>
2026-02-05 07:51:36 +0000

    fix test cases for bipartite

--- src/test_cases.py
+++ src/test_cases.py
@@ -137,6 +137,35 @@ def case8():
 
     return G, A
 
+reg_cases = [
+        (5, 10), (5, 100), (5, 150), (5, 200), 
+        (10, 20), (10, 100), (10, 150), (10, 200),
+        (20, 30), (30, 40), (40, 50), (50, 100),
+        (150, 200), (200, 250)
+    ] # regular graph cases of degree d and number of nodes n (d, n)
+
+bip_cases = [
+    10, 20, 30, 40, 50, 100, 150, 200, 250, 300
+]
+
+tree1 = Node("A")
+tree1.children = [Node("B")]
+k1 = 2
+
+tree2 = Node("A")
+tree2.children = [Node("B"), Node("C")]
+k2 = 3
+
+tree_sizes = [k1, k2] # sizes of trees
+
+# final homomorphism counts for k1 and k2
+k1_homs = []
+k2_homs = []
+
+k1_homs_gnn = []
+k2_homs_gnn = []
+
+
 def regular_tests():
     """
     Count number of homomorphisms on regular graphs
@@ -146,33 +175,10 @@ def regular_tests():
     Computes n * d^(k - 1)
     """
 
-    cases = [
-            (5, 10), (5, 100), (5, 150), (5, 200), 
-            (10, 20), (10, 100), (10, 150), (10, 200),
-            (20, 30), (30, 40), (40, 50), (50, 100),
-            (150, 200), (200, 250)
-        ] # regular graph cases of degree d and number of nodes n (d, n)
-
-    tree1 = Node("A")
-    tree1.children = [Node("B")]
-    k1 = 2
-    
-    tree2 = Node("A")
-    tree2.children = [Node("B"), Node("C")]
-    k2 = 3
-
-    tree_sizes = [k1, k2] # sizes of trees
-
-    # final homomorphism counts for k1 and k2
-    k1_homs = []
-    k2_homs = [] 
-
-    for case in cases:
+    for case in reg_cases:
         k1_homs.append(case[1] * case[0] ** (tree_sizes[0] - 1))
         k2_homs.append(case[1] * case[0] ** (tree_sizes[1] - 1))
 
-    k1_homs_gnn = []
-    k2_homs_gnn = []
 
     path = "../graphs/regular/"
 
@@ -191,7 +197,27 @@ def regular_tests():
     return comp1, comp2
 
 def bipartite_tests():
-    pass
+    for case in bip_cases:
+        k1_homs.append(2 * (case ** tree_sizes[0]))
+        k2_homs.append(2 * (case ** tree_sizes[1]))
+    
+    path = "../graphs/bipartite/"
+
+    for filename in os.listdir(path):
+        with open(f"{path}/{filename}", "rb") as f:
+            edges = pickle.load(f)
+
+            print(len(edges))
+
+        G = GNN(edges=edges, directed=False)
+        k1_homs_gnn.append(total_hom(tree1, G))
+        k2_homs_gnn.append(total_hom(tree2, G))
+
+    comp1 = k1_homs == k1_homs_gnn
+    comp2 = k2_homs == k2_homs_gnn
+
+
+    return comp1, comp2
 
 if __name__ == "__main__":
 
@@ -207,8 +233,13 @@ if __name__ == "__main__":
         print(homs)
     """
 
-    print(regular_tests()[0])
-    print(regular_tests()[1])
+    print(bipartite_tests()[0])
+    #print(bipartite_tests()[1])
+
+    #print(k1_homs)
+    #print(k1_homs_gnn)
+
+    #print("=" * 20)
 
 
     # nx.draw(G.g_disp, labels=label_dict, node_color=color_map, with_labels=True, font_weight='bold') drawes labels and colours
