344a0d1fd73c9362d3c6657be5f2a6a797fd3128
delterr <hasanahmadcodes@gmail.com>
2026-04-20 12:38:21 +0100

    make notebooks and make preprocessing step

--- src/homomorphism.py
+++ src/homomorphism.py
@@ -1,6 +1,40 @@
 from GNN import partition
 from collections import Counter
 
+
+def refine_colours(G):
+    """
+    Refine colours on a graph G
+    """
+    prev_partition = {}
+    same_partiton = False
+    i = 0
+    while not same_partiton:
+        A = G.update()
+        
+        colours = partition(A)
+        i += 1
+        
+        if colours == prev_partition:
+            same_partiton = True
+        else:
+            prev_partition = colours
+    
+    colour_count = Counter(A.values())
+
+    return A, colour_count
+
+def total_hom_preprocess(T, G, colour_count, A):
+    result = 0
+
+    for key1, value1 in colour_count.items():
+        for key2, value2 in A.items():
+            if key1 == value2:
+                node = key2
+        result += value1 * hom(T, G, node)
+    
+    return result
+
 def hom(T, G, x):
     """
     Compute homomorphism between tree T and graph G
